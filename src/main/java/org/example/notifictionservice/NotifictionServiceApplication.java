6d33885f2975832bbe4af090d448df39c48c9697
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-21 09:50:54 +0300

    Changed properties

--- src/main/java/org/example/notifictionservice/NotifictionServiceApplication.java
+++ src/main/java/org/example/notifictionservice/NotifictionServiceApplication.java
@@ -1,6 +1,6 @@
 package org.example.notifictionservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotifictionServiceApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("MAIL_USER", dotenv.get("MAIL_USER"));
-        System.setProperty("MAIL_PASS", dotenv.get("MAIL_PASS"));
-        System.setProperty("MAIL_HOST", dotenv.get("MAIL_HOST"));
-        System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
-
-        System.setProperty("RABBITMQ_USER", dotenv.get("RABBITMQ_USER"));
-        System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("MAIL_USER", dotenv.get("MAIL_USER"));
+//        System.setProperty("MAIL_PASS", dotenv.get("MAIL_PASS"));
+//        System.setProperty("MAIL_HOST", dotenv.get("MAIL_HOST"));
+//        System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
+//
+//        System.setProperty("RABBITMQ_USER", dotenv.get("RABBITMQ_USER"));
+//        System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
 
         SpringApplication.run(NotifictionServiceApplication.class, args);
     }
